54fabfef565db056eb4f54896dcbd363c26016ac
kleinesfilmroellchen <malu.bertsch@gmail.com>
2019-11-09 11:01:05 +0100

    add package information for all packages in the project
    re-enable bin's gitignore

--- src/klfr/sof/cli/package-info.java
+++ src/klfr/sof/cli/package-info.java
@@ -1,1 +1,5 @@
+/**
+ * Objects responsible for handling the command line interface (CLI) that SOF provides.<br><br>
+ * Within this package reside all the possible JVM execution entry points.
+ */
 package klfr.sof.cli;
--- src/klfr/sof/package-info.java
+++ src/klfr/sof/package-info.java
@@ -1,1 +1,5 @@
+/**
+ * The Stack with Objects and Functions (SOF) Reference implementation.<br><br>
+ * Currently, SOF is implemented with a full unoptimized interpreter.
+ */
 package klfr.sof;
