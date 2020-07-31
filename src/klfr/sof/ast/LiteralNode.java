54f334f2d9c4e2390d8f7d5687a04b4bf68ac0b4
kleinesfilmroellchen <malu.bertsch@gmail.com>
2020-07-31 16:23:05 +0200

    add copyright info to every file
    adjust license notice to be project-specific

--- src/klfr/sof/ast/LiteralNode.java
+++ src/klfr/sof/ast/LiteralNode.java
@@ -1,7 +1,6 @@
 package klfr.sof.ast;
 
 import java.util.*;
-import java.util.function.Consumer;
 
 import klfr.sof.lang.Stackable;
 import klfr.sof.lang.Stackable.DebugStringExtensiveness;
@@ -55,3 +54,21 @@ public class LiteralNode implements Node {
 	}
 
 }
+
+/*  
+The SOF programming language interpreter.
+Copyright (C) 2019-2020  kleinesfilmröllchen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
