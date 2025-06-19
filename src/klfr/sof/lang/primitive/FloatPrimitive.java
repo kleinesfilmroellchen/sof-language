83f0cec3ad6fc1e46645785ad3c0ec7545feb562
kleines Filmröllchen <kleines@filmroellchen.eu>
2025-06-19 22:12:44 +0200

    update to current versions

--- src/klfr/sof/lang/primitive/FloatPrimitive.java
+++ src/klfr/sof/lang/primitive/FloatPrimitive.java
@@ -186,7 +186,7 @@ public final class FloatPrimitive extends Primitive {
 	 * @param decimals number of decimals to round to. 0 = round to integer, negative = round to tens, 100's etc.
 	 * @return rounded number.
 	 */
-	public static strictfp double round(double d, int decimals) {
+	public static double round(double d, int decimals) {
 		return Math.round(d * Math.pow(10, decimals)) / Math.pow(10, decimals);
 	}
 
